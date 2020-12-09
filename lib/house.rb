a42dbe58e1a42abd7c5c0f3281df36538285aaa3
Dominic Padula <domopadula2192@gmail.com>
2020-12-09 14:10:26 -0700

    Finish iteration 4

--- lib/house.rb
+++ lib/house.rb
@@ -31,4 +31,30 @@ class House
   def details
     dets = {"price" => @price , "address" => @address}
   end
+
+  def price_per_square_foot
+    (@price.to_f / area).round(2)
+  end
+
+  def rooms_sorted_by_area
+    @rooms.sort_by do |room|
+      -room.area
+    end
+  end
+
+  def all_categories
+    all_categories = []
+      @rooms.each do |room|
+      all_categories << room.category
+    end
+    all_categories.uniq!
+  end
+
+  def rooms_by_category
+    breakdown = {}
+      all_categories.each do |category|
+      breakdown[category] = rooms_from_category(category)
+    end
+    breakdown 
+  end
 end

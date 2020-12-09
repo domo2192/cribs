37d0564aa7c988b9aa85aeb5dab1f62a035b93ca
Dominic Padula <domopadula2192@gmail.com>
2020-12-09 13:19:31 -0700

    Tests for iteration 2

--- test/house_test.rb
+++ test/house_test.rb
@@ -6,6 +6,8 @@ class HouseTest < Minitest::Test
 
   def setup
     @house = House.new("$400000", "123 sugar lane")
+    @room_1 = Room.new(:bedroom, 10, '13')
+    @room_2 = Room.new(:bedroom, 11, '15')
   end
 
   def test_it_exists_and_has_attributes
@@ -16,5 +18,11 @@ class HouseTest < Minitest::Test
 
   def test_rooms_is_empty_array
     assert_equal [], @house.rooms
+  end
+
+  def test_we_can_add_to_rooms
+    @house.add_room(@room_1)
+    @house.add_room(@room_2)
+    assert_equal [@room_1, @room_2], @house.rooms 
   end
 end
